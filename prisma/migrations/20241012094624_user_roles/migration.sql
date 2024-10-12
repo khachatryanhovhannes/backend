6c7b23c1412679697856fd1f82cb31463dc07740
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2024-10-12 13:52:16 +0400

    Adding Prisma model for Roles

---
+++ prisma/migrations/20241012094624_user_roles/migration.sql
@@ -0,0 +1,28 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `permissionId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `roles` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `roles_name_key`(`name`),
+    UNIQUE INDEX `roles_code_key`(`code`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_UserRoles` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
+
+    UNIQUE INDEX `_UserRoles_AB_unique`(`A`, `B`),
+    INDEX `_UserRoles_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `_UserRoles` ADD CONSTRAINT `_UserRoles_A_fkey` FOREIGN KEY (`A`) REFERENCES `roles`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_UserRoles` ADD CONSTRAINT `_UserRoles_B_fkey` FOREIGN KEY (`B`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
